c6c6e9d4d1cc7d0bf2c35ef19b04c4358dc07f1c
Victoria-Wei <77334874+Victoria-Wei@users.noreply.github.com>
2024-03-11 15:22:09 +0800

    Update path.sh

--- path.sh
+++ path.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-# 宣告 DACS_codeRoot 變數
-export DACS_codeRoot=/home/FedASR/dacs
-export DACS_dataRoot=/mnt/Internal/FedASR/Data/ADReSS-IS2020-data
-# 顯示 DACS_codeRoot 變數的值
-echo "DACS_codeRoot is set to: $DACS_codeRoot"
+# declar CPFL_codeRoot
+export CPFL_codeRoot=/home/FedASR/CPFL
+export CPFL_dataRoot=/mnt/Internal/FedASR/Data/ADReSS-IS2020-data
+# show CPFL_codeRoot
+echo "CPFL_codeRoot is set to: $CPFL_codeRoot"
